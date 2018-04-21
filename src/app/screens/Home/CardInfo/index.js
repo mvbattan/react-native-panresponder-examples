da5a9f29076bceedd2a726426c78b223e577b8b1
Manuel Battan <manuel.battan@wolox.com.ar>
2018-04-21 18:35:38 -0300

    Adding swipe gesture handlers

--- src/app/screens/Home/CardInfo/index.js
+++ src/app/screens/Home/CardInfo/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 import CustomButton from '../../../components/CustomButton';
 
 import styles from './styles';
 
 class CardInfo extends Component {
-  state = { scaleValue: new Animated.Value(0.8) };
+  state = {
+    scaleValue: new Animated.Value(0.8),
+    swipeValue: new Animated.Value(0)
+  };
+
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onStartShouldSetPanResponderCapture: () => false,
+    onMoveShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponderCapture: () => true,
+
+    onPanResponderGrant: () => true,
+    onPanResponderMove: Animated.event([null, { dx: this.state.swipeValue }]),
+    onPanResponderTerminationRequest: () => false,
+    onPanResponderRelease: (evt, gestureState) => {
+      // The user has released all touches while this view is the
+      // responder. This typically means a gesture has succeeded
+    },
+    onPanResponderTerminate: (evt, gestureState) => {
+      // Another component has become the responder, so this gesture
+      // should be cancelled
+    }
+  })
 
   handleButtonPress = () => {
     Animated.spring(this.state.scaleValue, { toValue: 1, useNativeEventDriver: true, friction: 1.6 }).start(
@@ -16,7 +38,12 @@ class CardInfo extends Component {
 
   render() {
     return (
-      <Animated.View style={{ transform: [{ scale: this.state.scaleValue }] }}>
+      <Animated.View
+        style={{
+          transform: [{ scale: this.state.scaleValue }, { translateX: this.state.swipeValue }]
+        }}
+        {...this.panResponder.panHandlers}
+      >
         <View style={styles.container} />
         <View style={styles.buttonContainer}>
           <CustomButton onPress={this.handleButtonPress} bold red title="DON'T" />
