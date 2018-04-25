d46d7d46826404b8111e9163c960a11d8968d4b8
Manuel Battan <manuel.battan@wolox.com.ar>
2018-04-25 11:50:46 -0300

    Adding some comments for panhandlers

--- src/app/screens/Home/components/DraggableView/index.js
+++ src/app/screens/Home/components/DraggableView/index.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { View, PanResponder } from 'react-native';
 
+// Taken a bit from https://gist.github.com/teameh/dd055d546a3bd8f85b9516840e3a45f3
 class DraggableView extends Component {
   state = {};
 
   panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: (evt, gestureState) =>
-      !console.log('onStartShouldSetPanResponder', { evt, gestureState }),
-    onStartShouldSetPanResponderCapture: (evt, gestureState) =>
-      !console.log('onStartShouldSetPanResponderCapture', { evt, gestureState }),
-    onMoveShouldSetPanResponder: (evt, gestureState) =>
-      !console.log('onMoveShouldSetPanResponder', { evt, gestureState }),
-    onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
-      !console.log('onMoveShouldSetPanResponderCapture', { evt, gestureState }),
+    // - Does this view want to become responder on the start of a touch?
+    onStartShouldSetPanResponder: (evt, gestureState) => {
+      console.log('onStartShouldSetPanResponder', { evt, gestureState });
+      return true;
+    },
 
+    // Should child views be prevented from becoming responder on first touch?
+    onStartShouldSetPanResponderCapture: (evt, gestureState) => {
+      console.log('onStartShouldSetPanResponderCapture', { evt, gestureState });
+      return true;
+    },
+
+    // - Called for every touch move on the View when it is not the responder
+    //   does this view want to "claim" touch responsiveness?
+    onMoveShouldSetPanResponder: (evt, gestureState) => {
+      console.log('onMoveShouldSetPanResponder', { evt, gestureState });
+      return true;
+    },
+
+    // Should child views be prevented from becoming responder of subsequent touches?
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+      console.log('onMoveShouldSetPanResponderCapture', { evt, gestureState });
+      return true;
+    },
+
+    // - The View is now responding for touch events.
     onPanResponderGrant: (evt, gestureState) => {
       // The gesture has started. Show visual feedback so the user knows
       // what is happening!
@@ -21,6 +39,7 @@ class DraggableView extends Component {
       // gestureState.d{x,y} will be set to zero now
       console.log('onPanResponderGrant', { evt, gestureState });
     },
+
     onPanResponderMove: (evt, gestureState) => {
       // The most recent move distance is gestureState.move{X,Y}
 
@@ -28,19 +47,26 @@ class DraggableView extends Component {
       // gestureState.d{x,y}
       console.log('onPanResponderMove', { evt, gestureState });
     },
-    onPanResponderTerminationRequest: (evt, gestureState) =>
-      console.log('onPanResponderTerminationRequest', { evt, gestureState }),
+
+    // - Something else wants to become responder.
+    //   Should this view release the responder? Returning true allows release
+    onPanResponderTerminationRequest: (evt, gestureState) => {
+      console.log('onPanResponderTerminationRequest', { evt, gestureState });
+      return false;
+    },
+
     onPanResponderRelease: (evt, gestureState) => {
       // The user has released all touches while this view is the
       // responder. This typically means a gesture has succeeded
       console.log('onPanResponderRelease', { evt, gestureState });
     },
+
     onPanResponderTerminate: (evt, gestureState) => {
       // Another component has become the responder, so this gesture
       // should be cancelled
       console.log('onPanResponderTerminate', { evt, gestureState });
     }
-  })
+  });
 
   render() {
     return (
